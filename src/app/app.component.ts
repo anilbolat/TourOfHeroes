bba0ecd8ce3620e82730bc5c3a593243a1208e4c
Anil Bolat <anilbolat90@gmail.com>
2017-10-17 22:27:24 +0300

    dependency injection in ap component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-{ id: 10, name: 'Raven' },
-{ id: 11, name: 'Mr. Nice' },
-{ id: 12, name: 'Narco' },
-{ id: 13, name: 'Bombasto' },
-{ id: 14, name: 'Celeritas' },
-{ id: 15, name: 'Magneta' },
-{ id: 16, name: 'RubberMan' },
-{ id: 17, name: 'Dynama' },
-{ id: 18, name: 'Dr IQ' },
-{ id: 19, name: 'Magma' },
-{ id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'my-app',
@@ -27,16 +15,6 @@ const HEROES: Hero[] = [
 	</li>
 	</ul>
 	<hero-detail [hero]="selectedHero"></hero-detail>
-	<div *ngIf="selectedHero">
-	<h2>{{selectedHero.name}} details!</h2>
-	<div>
-	<label>id: </label>{{selectedHero.id}}
-	</div>
-	<div>
-	<label>name: </label>
-	<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-	</div>
-	</div>
 	`,	
 	styles: [`
 	.selected {
@@ -86,15 +64,22 @@ const HEROES: Hero[] = [
 		margin-right: .8em;
 		border-radius: 4px 0 0 4px;
 	}
-	`]
+	`],
+	providers: [HeroService]
 })
 export class AppComponent  { 
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes: Hero[];
 	selectedHero: Hero;
+
+	constructor(private heroService: HeroService) { }  // dependency injection
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+
+	getHeroes(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
 
 }
